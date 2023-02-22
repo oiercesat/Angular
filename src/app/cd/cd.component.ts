6e5d4c00305f8e141e95bf28f918fadae4ef8180
Oier Cesat <ocesat@iutbayonne.univ-pau.fr>
2023-02-22 11:03:41 +0100

    reparation du commit initial

---
+++ src/app/cd/cd.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cd',
+  templateUrl: './cd.component.html',
+  styleUrls: ['./cd.component.scss']
+})
+export class CdComponent {
+
+}
